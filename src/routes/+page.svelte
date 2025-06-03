06f0183c62273ff7515f8a498a83cfac8bc2a3ca
iMD-on-GitHub <165696911+iMD-on-GitHub@users.noreply.github.com>
2025-06-02 22:41:47 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 <div class="relative flex min-h-dvh w-full flex-col">
   <nav class="sticky top-0 z-10 w-full bg-gray-700 px-5 drop-shadow-lg">
     <div class="mx-auto flex h-14 max-w-7xl items-center justify-between">
-      <img src={logo} alt="logo" class="h-6 sm:h-7" />
+      <img src={logo} alt="logo" class="h-14 sm:h-15" />
       <div class="mx-auto">
         <Balance />
       </div>
